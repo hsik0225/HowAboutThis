035b3d59b10aada12813610350e930c9693c6a0f
naru <hsik0225@gmail.com>
2020-09-03 21:32:25 +0900

    test : 기본 테스트 파일 정의

---
+++ src/test/java/com/naru/howaboutthis/HowaboutthisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naru.howaboutthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowaboutthisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
