321d3473422e8e6427f848347fed9af011ce07f8
naru <hsik0225@gmail.com>
2020-09-04 01:10:44 +0900

    feat : 예외처리 미구현, 약식으로 회원가입 기능 완성

--- src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
+++ src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
@@ -2,18 +2,33 @@ package com.naru.howaboutthis.user.controller;
 
 import com.naru.howaboutthis.user.domain.Policy;
 import com.naru.howaboutthis.user.domain.PolicySingleton;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.naru.howaboutthis.user.domain.User;
+import com.naru.howaboutthis.user.domain.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-
+@Slf4j
 @RequestMapping("/users")
 @RestController
 public class SignUpController {
+
+    private final UserRepository userRepository;
+
+    // 생성자 기반 DI
+    SignUpController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/policy")
     public Policy policy() {
         return PolicySingleton.getInstance();
     }
 
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
