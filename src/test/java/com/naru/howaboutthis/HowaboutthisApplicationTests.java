1176905a1e9ddef40bb3ae4b1acea32a6cd89b93
naru <hsik0225@gmail.com>
2020-09-03 22:28:03 +0900

    chore :  spring-boot-starter-web 의존성 추가
    @RestController를 사용하기 위해서 추가하였다

--- src/test/java/com/naru/howaboutthis/HowaboutthisApplicationTests.java
+++ src/test/java/com/naru/howaboutthis/HowaboutthisApplicationTests.java
@@ -2,12 +2,14 @@ package com.naru.howaboutthis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class HowaboutthisApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
