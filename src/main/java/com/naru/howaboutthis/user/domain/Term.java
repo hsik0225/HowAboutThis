e91522b6963584fd5d18dffa211a43033e7507ef
naru <hsik0225@gmail.com>
2020-09-03 22:29:02 +0900

    feat : 이용약관 항목 클래스 생성

---
+++ src/main/java/com/naru/howaboutthis/user/domain/Term.java
@@ -0,0 +1,12 @@
+package com.naru.howaboutthis.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Term {
+
+    String title;
+    String[] contents;
+}
