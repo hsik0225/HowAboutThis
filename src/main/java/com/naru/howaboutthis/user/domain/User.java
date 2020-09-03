568121b7bda96920067c3c1ed92a133b7d9ebe66
naru <hsik0225@gmail.com>
2020-09-04 01:10:07 +0900

    Feat : 유저 클래스 정의

---
+++ src/main/java/com/naru/howaboutthis/user/domain/User.java
@@ -0,0 +1,25 @@
+package com.naru.howaboutthis.user.domain;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+}
