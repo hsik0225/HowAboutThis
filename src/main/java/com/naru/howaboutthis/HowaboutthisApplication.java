8d68f64fe8d5d16aaf98a0692362b854bcf4e278
naru <hsik0225@gmail.com>
2020-09-03 21:33:00 +0900

    Feat : 어플리케이션 정의

---
+++ src/main/java/com/naru/howaboutthis/HowaboutthisApplication.java
@@ -0,0 +1,13 @@
+package com.naru.howaboutthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowaboutthisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowaboutthisApplication.class, args);
+    }
+
+}
