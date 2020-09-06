bb37dd814835c1d77cfaf9cd9fff254f06e95267
naru <hsik0225@gmail.com>
2020-09-07 01:52:52 +0900

    feat : 회원 가입 양식 작성 시 이메일 및 비밀번호 유효성 검증 구현

--- src/main/java/com/naru/howaboutthis/user/domain/User.java
+++ src/main/java/com/naru/howaboutthis/user/domain/User.java
@@ -1,11 +1,11 @@
 package com.naru.howaboutthis.user.domain;
 
+import com.naru.howaboutthis.user.controller.SignIn;
+import com.naru.howaboutthis.user.controller.SignUp;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Entity
 @Getter
@@ -28,8 +28,14 @@ public class User {
     @Column(name = "password")
     @NotEmpty
     @Size(min = 8, max = 16)
+
+    // 대/소 영문자, 숫자가 적어도 한 번은 포함되어야 한다
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*$")
     private String password;
 
     @Column(name = "name")
+    @Null(groups = SignIn.class)
+    @NotEmpty(groups = SignUp.class)
+    @Size(min = 2, max = 16, groups = SignUp.class)
     private String name;
 }
