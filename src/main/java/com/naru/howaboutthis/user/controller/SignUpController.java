175139c0af8555d6002ffd38d680646c7e063795
naru <hsik0225@gmail.com>
2020-09-07 02:35:17 +0900

    refactor : SignUpController의 의존성 주입을 Lombok#@RequiredArgsConstructor 로 변경

--- src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
+++ src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
@@ -1,36 +1,30 @@
 package com.naru.howaboutthis.user.controller;
 
-import com.naru.howaboutthis.exception.DuplicateEmailException;
 import com.naru.howaboutthis.user.domain.Policy;
 import com.naru.howaboutthis.user.domain.PolicySingleton;
 import com.naru.howaboutthis.user.domain.User;
 import com.naru.howaboutthis.user.service.SignUpService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Email;
 import javax.validation.groups.Default;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @Slf4j
+@RequiredArgsConstructor
 @Validated // @RequestParam 이나 @PatVariable을 메소드에서 Validation할 수 있게 한다
 @RequestMapping("/api/users")
 @RestController
 public class SignUpController {
 
-    private final SignUpService signupService;
-
-    public SignUpController(SignUpService signupService) {
-        this.signupService = signupService;
-    }
+    private final SignUpService signUpService;
 
     @GetMapping("/policy")
     public ResponseEntity<Policy> policy() {
@@ -39,7 +33,7 @@ public class SignUpController {
 
     @PostMapping
     public ResponseEntity<Void> signUp(@RequestBody @Validated({Default.class, SignUp.class}) User user) throws URISyntaxException {
-        signupService.save(user);
+        signUpService.save(user);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(new URI("/api/main"));
@@ -49,31 +43,7 @@ public class SignUpController {
 
     @GetMapping("/{email}")
     public ResponseEntity<Void> canUseEmail(@PathVariable @Email String email) {
-        signupService.isDuplicated(email);
+        signUpService.isDuplicated(email);
         return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @ExceptionHandler(DuplicateEmailException.class)
-    public ResponseEntity<String> handleDuplicateEmailException(DuplicateEmailException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        StringBuilder sb = new StringBuilder();
-        for (FieldError error : e.getBindingResult().getFieldErrors()) {
-            sb.append(error.getField()).append("의 값을 잘못 입력하셨습니다\n");
-            sb.append(error.getDefaultMessage()).append("\n\n");
-        }
-        return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-        StringBuilder sb = new StringBuilder();
-        for (String error : e.getMessage().split(",")) {
-            sb.append(error.trim()).append("\n");
-        }
-        return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
     }
 }
