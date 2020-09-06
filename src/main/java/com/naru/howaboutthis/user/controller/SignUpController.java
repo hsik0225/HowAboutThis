b9ad64eecc33ef29536a03245c3923120306a91b
naru <hsik0225@gmail.com>
2020-09-07 00:44:07 +0900

    feat : 회원가입시 이메일 및 비밀번호 Validation 기능 구현

--- src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
+++ src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
@@ -8,9 +8,17 @@ import com.naru.howaboutthis.user.service.SignUpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 @Slf4j
+@Validated // @RequestParam 이나 @PatVariable을 메소드에서 Validation할 수 있게 한다
 @RequestMapping("/api/users")
 @RestController
 public class SignUpController {
@@ -27,13 +35,13 @@ public class SignUpController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> signUp(@RequestBody User user) {
+    public ResponseEntity<Void> signUp(@RequestBody @Valid User user) {
         signupService.save(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/{email}")
-    public ResponseEntity<Void> canUseEmail(@PathVariable String email) {
+    public ResponseEntity<Void> canUseEmail(@PathVariable @Min(4) String email, @RequestParam @Min(4) String test) {
         signupService.isDuplicated(email);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -41,5 +49,24 @@ public class SignUpController {
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<String> handleDuplicateEmailException(DuplicateEmailException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringBuilder sb = new StringBuilder();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            sb.append(error.getField()).append("의 값을 잘못 입력하셨습니다\n");
+            sb.append(error.getDefaultMessage()).append("\n\n");
+        }
+        return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        StringBuilder sb = new StringBuilder();
+        for (String error : e.getMessage().split(",")) {
+            sb.append(error.trim()).append("\n");
+        }
+        return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/naru/howaboutthis/user/domain/User.java
+++ src/main/java/com/naru/howaboutthis/user/domain/User.java
@@ -3,6 +3,9 @@ package com.naru.howaboutthis.user.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -17,10 +20,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(name = "email")
+    @Email
     private String email;
 
+    @Column(name = "password")
+    @NotEmpty
+    @Size(min = 8, max = 16)
     private String password;
 
+    @Column(name = "name")
     private String name;
 }
