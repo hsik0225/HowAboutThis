9ab3e3123903c8f60c664b2a36009d09239c7bad
naru <hsik0225@gmail.com>
2020-09-07 02:25:50 +0900

    refactor : 회원가입 및 로그인 성공시 메인화면으로 리다이렉트하게 수정

--- src/main/java/com/naru/howaboutthis/user/controller/SignInController.java
+++ src/main/java/com/naru/howaboutthis/user/controller/SignInController.java
@@ -3,6 +3,7 @@ package com.naru.howaboutthis.user.controller;
 import com.naru.howaboutthis.user.domain.User;
 import com.naru.howaboutthis.user.service.SignInService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.groups.Default;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,8 +22,12 @@ public class SignInController {
     private final SignInService signInService;
 
     @PostMapping("/api/sign-in")
-    public ResponseEntity<Void> signIn(@RequestBody @Validated({Default.class, SignIn.class}) User user) {
+    public ResponseEntity<Void> signIn(@RequestBody @Validated({Default.class, SignIn.class}) User user) throws URISyntaxException {
         signInService.signIn(user);
-        return new ResponseEntity<>(HttpStatus.FOUND);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(new URI("/api/main"));
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
--- src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
+++ src/main/java/com/naru/howaboutthis/user/controller/SignUpController.java
@@ -6,6 +6,7 @@ import com.naru.howaboutthis.user.domain.PolicySingleton;
 import com.naru.howaboutthis.user.domain.User;
 import com.naru.howaboutthis.user.service.SignUpService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Email;
 import javax.validation.groups.Default;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Slf4j
 @Validated // @RequestParam 이나 @PatVariable을 메소드에서 Validation할 수 있게 한다
@@ -35,9 +38,13 @@ public class SignUpController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> signUp(@RequestBody @Validated({Default.class, SignUp.class}) User user) {
+    public ResponseEntity<Void> signUp(@RequestBody @Validated({Default.class, SignUp.class}) User user) throws URISyntaxException {
         signupService.save(user);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(new URI("/api/main"));
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
     @GetMapping("/{email}")
