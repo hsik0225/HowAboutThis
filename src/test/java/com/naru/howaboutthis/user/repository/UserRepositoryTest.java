df32451d2f2445a090ad733ac26907ca84bf4ed3
naru <hsik0225@gmail.com>
2020-09-06 12:10:04 +0900

    feat : 유저 저장 및 단일 조회 기능 테스트 구현

---
+++ src/test/java/com/naru/howaboutthis/user/repository/UserRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.naru.howaboutthis.user.repository;
+
+import com.naru.howaboutthis.user.domain.User;
+import com.naru.howaboutthis.user.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        testUser = userRepository.save(
+                User.builder()
+                .name("naru")
+                .email("test@naver.com")
+                .password("test1234")
+                .build()
+        );
+    }
+
+    @Test
+    public void 회원가입_테스트() {
+        // then
+        assertThat(testUser.getName()).isEqualTo("naru");
+        assertThat(testUser.getEmail()).isEqualTo("test@naver.com");
+        assertThat(testUser.getPassword()).isEqualTo("test1234");
+    }
+
+    @Test
+    @DisplayName("단일_조회_테스트")
+    void 단일_조회_테스트() {
+        // given
+        String email = "test@naver.com";
+
+        // when
+        User foundUser = userRepository.findByEmail(email);
+
+        // then
+        assertThat(foundUser).isNotNull();
+    }
+}
