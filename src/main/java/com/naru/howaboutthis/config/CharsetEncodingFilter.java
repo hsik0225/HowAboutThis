e0bd6135c9bf48cc34bcdc7baec5dbc14bcdadbc
naru <hsik0225@gmail.com>
2020-09-10 00:54:00 +0900

    feat : CharsetEncodingFilter에 클래스 설명 주석 추가

--- src/main/java/com/naru/howaboutthis/config/CharsetEncodingFilter.java
+++ src/main/java/com/naru/howaboutthis/config/CharsetEncodingFilter.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import javax.servlet.*;
 import java.io.IOException;
 
+/**
+ * 요청과 응답의 인코딩 값을 UTF-8로 변경하기 위한 클래스(기본 = ISO-8859-1)
+ */
 @Component
 public class CharsetEncodingFilter implements Filter {
 
