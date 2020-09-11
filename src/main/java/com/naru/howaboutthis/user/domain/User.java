50b443db1c94b9fb65281f216879457067ef8137
naru <hsik0225@gmail.com>
2020-09-11 16:45:34 +0900

    feat : User의 password에 groups 속성 추가 및 password 암호화 기능 구현
    - validation이 repository에 persist할 때도 실행되어 groups를 SignUp으로 설정하여 컨트롤러에서만 validation이 실행되게 설정하였다
    - password 암호화 기능을 구현하여 DB에 저장할 때 암호화된 비밀번호를 저장한다

--- src/main/java/com/naru/howaboutthis/user/domain/User.java
+++ src/main/java/com/naru/howaboutthis/user/domain/User.java
@@ -3,6 +3,7 @@ package com.naru.howaboutthis.user.domain;
 import com.naru.howaboutthis.user.controller.SignIn;
 import com.naru.howaboutthis.user.controller.SignUp;
 import lombok.*;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
@@ -27,10 +28,10 @@ public class User {
 
     @Column(name = "password")
     @NotEmpty
-    @Size(min = 8, max = 16)
+    @Size(min = 8, max = 16, groups = SignUp.class)
 
     // 대/소 영문자, 숫자가 적어도 한 번은 포함되어야 한다
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*$")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*$", groups = SignUp.class)
     private String password;
 
     @Column(name = "name")
@@ -38,4 +39,8 @@ public class User {
     @NotEmpty(groups = SignUp.class)
     @Size(min = 2, max = 16, groups = SignUp.class)
     private String name;
+
+    public void hashPassword(User user) {
+        this.password = BCrypt.hashpw(user.password, BCrypt.gensalt());
+    }
 }
