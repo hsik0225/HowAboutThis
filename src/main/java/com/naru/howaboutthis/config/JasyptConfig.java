fdb889d7035d8e3438047dee48bf05f9f0c35e03
naru <hsik0225@gmail.com>
2020-09-14 14:13:36 +0900

    feat : 암호화 및 복호화해주는 Jasypt Bean 작성

---
+++ src/main/java/com/naru/howaboutthis/config/JasyptConfig.java
@@ -0,0 +1,33 @@
+package com.naru.howaboutthis.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${ENCRYPTOR_PASSWORD}")
+    private String password;
+
+    @Value("${ENCRYPTOR_ALGORITHM}")
+    private String algorithm;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm(algorithm);
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
