4d013f74a443160b724b31cffba4dab55c7ee2eb
naru <hsik0225@gmail.com>
2020-09-17 20:22:29 +0900

    refactor : 유저 이메일 중복 체크 테스트 메소드 수정
    서비스의 어떤 메소드가 어떤 예외처리를 하는지 더 명확하게 드러나기 위해 SignUpService의 의존성을 주입받고
    signUpService#checkDuplicateEmail()을 assertThrows하였다

--- src/test/java/com/naru/howaboutthis/user/service/SignUpServiceTest.java
+++ src/test/java/com/naru/howaboutthis/user/service/SignUpServiceTest.java
@@ -1,5 +1,6 @@
 package com.naru.howaboutthis.user.service;
 
+import com.naru.howaboutthis.exception.DuplicateEmailException;
 import com.naru.howaboutthis.user.domain.User;
 import com.naru.howaboutthis.user.domain.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /*
 @DataJpaTest를 사용하고 싶었으나 실제 DB를 사용하기 위해 DB의 정보를
@@ -18,6 +20,9 @@ class SignUpServiceTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private SignUpService signUpService;
 
     @Test
     @Transactional
@@ -40,7 +45,11 @@ class SignUpServiceTest {
     @Transactional
     public void 중복_이메일_체크_테스트() {
         회원가입_테스트();
-        boolean exists = userRepository.existsByEmail("test@gmail.com");
-        assertThat(exists).isEqualTo(true);
+        DuplicateEmailException exception = assertThrows(
+                DuplicateEmailException.class,
+                () -> signUpService.checkDuplicateEmail("test@gmail.com")
+        );
+
+        assertThat(exception.getMessage()).isEqualTo("중복된 이메일입니다");
     }
 }
