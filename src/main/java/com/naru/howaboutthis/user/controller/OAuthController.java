882836d766acf7be9e82c4ffd2db097797435b02
naru <hsik0225@gmail.com>
2020-09-17 13:52:17 +0900

    feat : OAuth 로그인 컨트롤러 구현

---
+++ src/main/java/com/naru/howaboutthis/user/controller/OAuthController.java
@@ -0,0 +1,50 @@
+package com.naru.howaboutthis.user.controller;
+
+import com.naru.howaboutthis.user.domain.GitHubProperty;
+import com.naru.howaboutthis.user.domain.GitHubUrl;
+import com.naru.howaboutthis.user.service.SignUpService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/oauth")
+@RestController
+public class OAuthController {
+
+    private final SignUpService signUpService;
+
+    private final GitHubProperty gitHubProperty;
+
+    @GetMapping("/code")
+    public ResponseEntity<Object> getCode() {
+        HttpHeaders headers = new HttpHeaders();
+        URI uri = UriComponentsBuilder.fromUriString(GitHubUrl.CODE.getUrl())
+                .queryParam("client_id", gitHubProperty.getClientId())
+                .queryParam("scope", "user")
+                .build()
+                .toUri();
+
+        headers.setLocation(uri);
+
+        return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Object> oauth(@RequestParam String code, HttpServletResponse response) throws IOException {
+        response.sendRedirect("/api/users/policy");
+        return new ResponseEntity<>("OAuth Success", HttpStatus.FOUND);
+    }
+}
