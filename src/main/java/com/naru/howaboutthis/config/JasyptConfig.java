9306bb48a7618020ecfe2cbf166fab34602e7945
naru <hsik0225@gmail.com>
2020-09-17 18:28:33 +0900

    refactor : Encryptor 비밀번호를 OS의 환경변수로 관리하도록 수정 및 그에 따른 변경

--- src/main/java/com/naru/howaboutthis/config/JasyptConfig.java
+++ src/main/java/com/naru/howaboutthis/config/JasyptConfig.java
@@ -3,25 +3,20 @@ package com.naru.howaboutthis.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
 
-    @Value("${ENCRYPTOR_PASSWORD}")
-    private String password;
-
-    @Value("${ENCRYPTOR_ALGORITHM}")
-    private String algorithm;
+    private static final String password = System.getenv("ENCRYPTOR_PASSWORD");
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(password);
-        config.setAlgorithm(algorithm);
+        config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
         config.setProviderName("SunJCE");
